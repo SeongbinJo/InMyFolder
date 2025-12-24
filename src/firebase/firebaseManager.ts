7de99335e3be8adcb5d53a64f61386a044ce22c8
avan31017 <avan31017@gmail.com>
2025-12-24 18:59:25 +0900

    add code - add/fetch user data in firebaseManager.ts

---
+++ src/firebase/firebaseManager.ts
@@ -0,0 +1,72 @@
+import type { User } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "./firebaseConfig";
+
+type UserInfo = {
+    uid: string;
+    email: string | null;
+    displayName: string | null;
+    createdAt: Date;
+    portfolio: [name: string, createdAt: Date, Photos[], Texts[]][];
+}
+
+type Photos = {
+    name: string;
+    url: string;
+    position: position;
+}
+
+type Texts = {
+    name: string;
+    font: [fontTitle: string, fontSize: number];
+    position: position;
+}
+
+type position = [x: number, y: number];
+
+
+// 신규 유저 데이터 추가
+export async function addUserData(userInfo: User) {
+    // 신규 유저 default 데이터
+    const defaultData: UserInfo = {
+        uid: userInfo.uid,
+        email: userInfo.email,
+        displayName: userInfo.displayName,
+        createdAt: new Date(),
+        portfolio: []
+    }
+
+    try {
+        await setDoc(doc(db, "users", userInfo.uid), defaultData)
+    } catch (error) {
+        console.error("Error adding user data: ", error);
+    }
+}
+
+// 기존 유저 데이터 불러오기
+export async function fetchUserData(uid: string) {
+    try {
+        const docRef = doc(db, "users", uid)
+        const snap = await getDoc(docRef)
+
+        if (!snap.exists()) {
+            console.log("해당 유저의 문서가 존재하지 않습니다.")
+            return null
+        }
+
+        const data = snap.data()
+
+        const userData: UserInfo = {
+            uid: data.uid,
+            email: data.email,
+            displayName: data.displayName,
+            createdAt: data.createdAt.toDate(),
+            portfolio: data.portfolio
+        }
+
+        return userData
+    } catch (error) {
+        console.error("Error fetching user data: ", error);
+        return null
+    }
+}
